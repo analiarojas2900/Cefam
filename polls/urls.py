bd3c4bd05bfd58858b5e0101a024e86793df2443
Jeremaia <xyerkox99@gmail.com>
2022-05-13 12:53:15 -0400

    'funcional'_relativamente

--- polls/urls.py
+++ polls/urls.py
@@ -6,12 +6,13 @@ from . import views
 
 urlpatterns = [
     path('', views.iniciar_sesion, name='iniciar_sesion'),
-    path('farmaceutico_home/', views.farmaceutico_home, name='farmaceutico_home'),
-    path('farmaceutico_revisar_receta/', views.farmaceutico_revisar_receta, name='farmaceutico_revisar_receta'),
+    path('admin_creacion/', views.admin_creacion, name='admin_creacion'),
+    path('farm_home/', views.farm_home, name='farm_home'),
+    path('farm_revisar_receta/', views.farm_revisar_receta, name='farm_revisar_receta'),
+    path('farm_modificar_stock/', views.farm_modificar_stock, name='farm_modificar_stock'),
     path('medico_home/', views.medico_home, name='medico_home'),
     path('medico_receta_medica/', views.medico_receta_medica, name='medico_receta_medica'),
     path('desconectar/', views.desconectar, name= 'desconectar'),
-    path('admin_creacion/', views.admin_creacion, name='admin_creacion'),
 ]
 
 if settings.DEBUG:
--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,19 @@
-from re import template
 from django.shortcuts import render, redirect
-from .models import Paciente, CustomUsuario, Personal, Medicamento
+from .models import Entrega_medicamentos, Ficha_paciente, Paciente, CustomUsuario, Personal, Medicamento, Receta_medica
 from django.contrib.auth import authenticate, login, logout
-from .forms import creacion_personal
+from .forms import creacion_personal, creacion_receta, entrega_medicamento, modificar_stock
 from django.contrib import messages
 from django.db.models import Q
 
 # Create your views here.
 
+#Identificacion ------------------------------------------------------------------
+
 def iniciar_sesion(request):
     if request.user.is_authenticated and request.user.is_medico:
         return redirect(medico_home)
     elif request.user.is_authenticated and request.user.is_farmaceutico:
-        return redirect(farmaceutico_home)
+        return redirect(farm_home)
     elif request.user.is_authenticated:
         if request.user.is_admin or request.user.is_staff:
             return redirect(admin_creacion)
@@ -29,7 +30,7 @@ def iniciar_sesion(request):
                 return redirect(medico_home)
             elif user.is_farmaceutico:
                 login(request, user)
-                return redirect(farmaceutico_home)
+                return redirect(farm_home)
             elif user.is_admin or user.is_staff:
                 login(request, user)
                 return redirect(admin_creacion)
@@ -42,34 +43,166 @@ def iniciar_sesion(request):
 
     return render(request, 'registration/iniciar_sesion.html')
 
-def farmaceutico_home(request):
+def desconectar(request):
+    logout(request)
+    return redirect('iniciar_sesion')    
 
+
+
+
+#Farmaceutico ------------------------------------------------------------------
+
+def farm_home(request):
     buscar_paciente = request.GET.get('buscador_paciente')
     medicamento = Medicamento.objects.all()
+    paciente = Paciente.objects.all()
+    verif = False
 
-    if buscar_paciente:
+    if buscar_paciente and Paciente.objects.filter(Q(run_paciente = buscar_paciente)):
         paciente = Paciente.objects.filter(Q(run_paciente = buscar_paciente))
-    else:
-        paciente = Paciente.objects.filter(Q(run_paciente = '1'))
+        verif = True
 
-    return render(request, 'html/farmaceutico_home.html', {'paciente':paciente, 'medicamento':medicamento})
+    data = {
+        'paciente':paciente, 
+        'medicamento': medicamento, 
+        'verif':verif
+    }
 
-def farmaceutico_revisar_receta(request):
-    return render(request, 'html/farmaceutico_revisar_receta.html')
+    return render(request, 'farmaceutico/farm_home.html', data)
+
+def farm_revisar_receta(request):
+    buscar_receta = request.GET.get('buscador_receta')
+    form_entrega = entrega_medicamento(request.POST, request.FILES)
+    medicamento = Medicamento.objects.all()
+    receta = Receta_medica.objects.all()
+    verif = False
+    
+    if buscar_receta and Receta_medica.objects.filter(Q(id = buscar_receta)):
+        receta = Receta_medica.objects.filter(Q(id = buscar_receta))
+        verif = True
+    
+    if form_entrega.is_valid and Medicamento.objects.filter(Q(id = form_entrega.data.get('id_medicamento'))):
+        ficha = Ficha_paciente.objects.filter(Q(id = form_entrega.data.get('id_ficha')))
+        personal = Personal.objects.filter(Q(id = form_entrega.data.get('id_personal')))
+        receta_upd = Receta_medica.objects.filter(Q(id = form_entrega.data.get('id_receta')))
+        medic = Medicamento.objects.filter(Q(id = form_entrega.data.get('id_medicamento')))
+        for m in medic:
+            for r in receta_upd:
+                cant_medic = m.cantidad_medicamento
+                cant_medic = int(cant_medic)
+                cant_entr = form_entrega.data.get('cant_entregada')
+                cant_entr = int(cant_entr)
+                cant_a_entr = r.cantidad_medicamentos
+                cant_a_entr = int(cant_a_entr)
+                cant_total_medic = cant_medic - cant_entr
+                cant_total_rec = cant_a_entr -  cant_entr
+
+        for f in ficha:
+            for p in personal:
+                for r in receta_upd:
+                    entrega = Entrega_medicamentos(cantidad_entregada = form_entrega.data.get('cant_entregada'),
+                    id_ficha = f,
+                    id_personal = p,
+                    id_receta = r)
+                    entrega.save()
+        
+        medic = medic.update(cantidad_medicamento = str(cant_total_medic))
+        receta_upd = receta_upd.update(cantidad_medicamentos = str(cant_total_rec))
+
+    data = {
+        'form_entrega': entrega_medicamento,
+        'receta': receta, 
+        'medicamento': medicamento, 
+        'verif': verif
+    }
+
+    return render(request, 'farmaceutico/farm_revisar_receta.html', data)
+
+def farm_modificar_stock(request):
+    medicamento = Medicamento.objects.all()
+
+    if request.POST:
+        form_modificacion = modificar_stock(request.POST, request.FILES)
+        id_medicamento = request.POST.get('id_medicamento')
+        if form_modificacion.is_valid:
+            medicamento = medicamento.filter(id = id_medicamento)
+            nombre = form_modificacion.data.get('nombre_medicamento')
+            fecha_elab = form_modificacion.data.get('fecha_elab_medicamento')
+            fecha_venc = form_modificacion.data.get('fecha_venc_medicamento')
+            cantidad = form_modificacion.data.get('cantidad_medicamento')
+            if nombre:
+                medicamento.update(nombre_medicamento = nombre)
+            if fecha_elab:
+                medicamento.update(fecha_elab_medicamento = fecha_elab)
+            if fecha_venc:
+                medicamento.update(fecha_venc_medicamento = fecha_venc)
+            if cantidad:
+                medicamento.update(cantidad_medicamento = cantidad)
+
+    medicamento = Medicamento.objects.all()
+
+    data = {
+        'form_modificacion': modificar_stock,
+        'medicamento': medicamento,
+    }
+    return render(request, 'farmaceutico/farm_modificar_stock.html', data)
+
+
+
+
+#Medico ------------------------------------------------------------------
 
 def medico_home(request):
-    return render(request, 'html/medico_home.html')
+    buscar_paciente = request.GET.get('buscador_paciente')
+    medicamento = Medicamento.objects.all()
+    paciente = Paciente.objects.all()
+    verif = False
+
+    if buscar_paciente and Paciente.objects.filter(Q(run_paciente = buscar_paciente)):
+        paciente = Paciente.objects.filter(Q(run_paciente = buscar_paciente))
+        verif = True
+
+    data = {
+        'paciente':paciente, 
+        'medicamento': medicamento, 
+        'verif':verif
+    }
+
+    return render(request, 'medico/medico_home.html', data)
 
 def medico_receta_medica(request):
-    return render(request, 'html/medico_receta_medica.html')
+    if request.POST:
+        form = creacion_receta(request.POST, request.FILES)
+        if form.is_valid():
+            personal = Personal.objects.filter(Q(id = form.cleaned_data.get('id_personal')))
+            medicamento = Medicamento.objects.filter(Q(id = form.cleaned_data.get('id_medicamento')))
+            paciente = Paciente.objects.filter(Q(id = form.cleaned_data.get('id_paciente')))
+            for p in personal:
+                for m in medicamento:
+                    for pa in paciente:
+                        receta_medica = Receta_medica(prescripcion_receta = form.cleaned_data.get('prescripcion_receta'),
+                        fecha_receta = form.cleaned_data.get('fecha_receta'),
+                        cantidad_medicamentos = form.cleaned_data.get('cantidad_medicamento'),
+                        id_personal = p,
+                        id_medicamento = m,
+                        id_paciente = pa
+                        )
+            receta_medica.save()
+        return redirect(medico_home)
+    
+    data = {
+        'form': creacion_receta,
+    }
+        
+    return render(request, 'medico/medico_receta_medica.html', data)
+
+
+
+
+#Creacion Admin ------------------------------------------------------------------
 
 def admin_creacion(request):
     if request.POST:
-        nombre = ' '
-        apellido = ' '
-        run = ' '
-        dv = ' '
-        tipo = ' '
         form = creacion_personal(request.POST, request.FILES)
         print(request.FILES)
         if form.is_valid():
@@ -107,8 +240,4 @@ def admin_creacion(request):
             messages.success(request, 'Usuario creado correctamente, su cuenta: \nnombre: ' + nombre[0:2] + '.' + apellido +
             '\ncontraseña: '+ apellido[0].capitalize() + str(run) + '-' + str(dv))
         return redirect(admin_creacion)
-    return render(request, 'html/admin_creacion.html', {'form': creacion_personal})
-
-def desconectar(request):
-    logout(request)
-    return redirect('iniciar_sesion')    
+    return render(request, 'admin/admin_creacion.html', {'form': creacion_personal})
